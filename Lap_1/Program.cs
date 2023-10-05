95c05527d0fd3ae8f0ddd2add5063afae3bd960b
GlankS21 <asgar21502@gmail.com>
2023-10-05 09:58:33 +0300

    init

---
+++ Lap_1/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Lap_1;
+
+public class Program
+{
+    static void Sort(double[] arr){
+        var fist = new double[arr.Length];
+        var second = new double[arr.Length];
+        // ReSharper disable once InconsistentNaming
+        var fi_index = 0;
+        var se_index = 0;
+        for (int i = 0; i < arr.Length; i++){
+            if (Math.Abs(arr[i]) < 1) fist[fi_index++] = arr[i];
+            else second[se_index++] = arr[i];
+        }
+        Array.Copy(fist, 0, arr, 0, fi_index);
+        Array.Copy(second, 0, arr, fi_index, se_index);
+    }
+    static int Min(double[] arr){
+        var min_index = 0;
+        for (var i = 1; i < arr.Length; ++i){
+            if (arr[i] < arr[min_index]) min_index = i;
+        }
+        return min_index;
+    }
+    static double sum_elements(double[] arr){
+        var fi_index = int.MinValue; // найти первый отрицательный элемент
+        for (var i = 0; i < arr.Length; ++i){
+            if (arr[i] < 0){
+                fi_index = i;
+                break;
+            }
+        }
+        if (fi_index != int.MinValue){ // найти второй отрицательный элемент
+            var se_index = int.MinValue;
+            for (var i = fi_index + 1; i < arr.Length; ++i){
+                if (arr[i] < 0){
+                    se_index = i;
+                    break;
+                }
+            }
+            if (se_index != int.MinValue){ // найти сумму 
+                var sum = 0.0;
+                for (var i = fi_index + 1; i < se_index; ++i) sum += arr[i];
+                return sum;
+            }
+        } 
+        return Double.MinValue;
+    }
+    static void Main(){
+        var n = int.Parse(Console.ReadLine());
+        var arr = new double[n];
+        for (var i = 0; i < arr.Length; ++i)
+            arr[i] = double.Parse(Console.ReadLine());
+        // сортировать массив 
+        Sort(arr);
+        Console.Write("Matrix : ");
+        for (var i = 0; i < arr.Length; ++i) Console.Write(arr[i] + " ");
+        Console.WriteLine();
+
+        // number of the minimum array element 
+        Console.WriteLine("number of the minimum array element: " + Min(arr));
+
+        // сумму элементов массива, расположенных между первым и вторым отрицательными элементами.
+        double sum = sum_elements(arr);
+        Console.WriteLine(((sum == Double.MinValue) ? "Error":"the sum of the array elements located between the first and second negative elements: " + sum));
+    }
+}
---
+++ Lap_1_2/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace  Lap_1_2;
+
+/*
+ * Характеристикой столбца целочисленной матрицы назовем сумму модулей его отрицательных нечетных элементов.
+ * Переставляя столбцы заданной матрицы, расположить их в соответствии с ростом характеристик.
+ * Найти сумму элементов в тех столбцах, которые содержат хотя бы один отрицательный элемент
+ */
+
+public class Program
+{
+    // Поменяйте местами два столбца массива
+    static void swap(int[,] arr, int a, int b) {
+        for (var i = 0; i < arr.GetLength(0); ++i) {
+            var temp = arr[i, a];
+            arr[i, a] = arr[i, b];
+            arr[i, b] = temp;
+        }
+    }
+    // Вычислить сумму отрицательных элементов столбцов
+    static int[] sub_sum_arr(int[,] arr) {
+        var sum = new int[arr.GetLength(1)];
+        for (var j = 0; j < arr.GetLength(1); ++j) {
+            for (var i = 0; i < arr.GetLength(0); ++i) {
+                sum[j] += (arr[i, j] < 0) ? Math.Abs(arr[i, j]) : 0;
+            }
+        }
+        return sum;
+    }
+    // сортировать массив 
+    static void sort_array(int[,] arr)
+    {
+        // найти сумму 
+        var sum = sub_sum_arr(arr);
+        // сортировать массив 
+        for (var i = 0; i < sum.Length; ++i) {
+            for (var j = i; j < sum.Length; ++j) {
+                if (sum[i] > sum[j]) {
+                    swap(arr, i, j);
+                    var temp = sum[i];
+                    sum[i] = sum[j];
+                    sum[j] = temp;
+                }
+            }
+            
+            
+            
+            
+        }
+    }
+
+    static int sum_array(int[,] arr)
+    {
+        var sum_arr = sub_sum_arr(arr);
+        var sum = 0;
+        for (var j = 0; j < arr.GetLength(1); ++j) {
+            if (sum_arr[j] != 0) {
+                for (var i = 0; i < arr.GetLength(0); ++i) {
+                    sum += arr[i, j];
+                }
+            }
+        }
+        return sum;
+    }
+    // print
+    static void print_array(int[,] arr) {
+        for (var i = 0; i < arr.GetLength(0); ++i){
+            for (var j = 0; j < arr.GetLength(1); ++j){
+                Console.Write($"{arr[i,j],5:D}");
+            }
+            Console.WriteLine();
+        }
+    }
+    static void Main(String[] args)
+    {
+        // создать массив 
+        var n = int.Parse(Console.ReadLine());
+        var m = int.Parse(Console.ReadLine());
+        var arr = new int[n,m];
+        Random r = new Random();
+        for (var i = 0; i < n; ++i) {
+            for (var j = 0; j < m; ++j){
+                arr[i, j] = r.Next(-100,100);
+            }
+        }
+        Console.WriteLine("original array");
+        print_array(arr);
+        // сортировать 
+        sort_array(arr);
+        Console.WriteLine("array after sorting");
+        print_array(arr);
+        // найти сумму 
+        Console.WriteLine("sum of columns with at least 1 negative element: " + sum_array(arr));
+
+    }
+}
+
